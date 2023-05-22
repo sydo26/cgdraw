06cdccd54a69c02b3092fc8b7f5f15cacbebb21f
Vinicius <sydoafk@gmail.com>
2023-05-22 12:00:54 -0300

    feat: add key_code in parameter of the KeyPressed App Event

--- crates/cgdraw_app/src/events.rs
+++ crates/cgdraw_app/src/events.rs
@@ -1,10 +1,12 @@
+use cgdraw_core::keyboard::keys::VirtualKeyCode;
+
 pub enum AppEvent {
     Setup,
     Finished,
     Update,
     Draw,
-    Keydown,
-    Keypress,
+    KeyDown,
+    KeyPressed { key_code: VirtualKeyCode },
     Keyup,
     Resize { width: u32, height: u32 },
 }
