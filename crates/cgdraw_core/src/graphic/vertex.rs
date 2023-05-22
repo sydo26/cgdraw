bc431c368368aaf1e1576d82d99bc2334d784ce9
Vinicius <sydoafk@gmail.com>
2023-05-22 12:52:16 -0300

    fix: insert desc in vertex impl

--- crates/cgdraw_core/src/graphic/vertex.rs
+++ crates/cgdraw_core/src/graphic/vertex.rs
@@ -5,25 +5,27 @@ pub struct Vertex {
     pub color: [f32; 3],
 }
 
-fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-    use std::mem;
+impl Vertex {
+    pub fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        use std::mem;
 
-    wgpu::VertexBufferLayout {
-        array_stride: mem::size_of::<SimpleVertex>() as wgpu::BufferAddress,
-        step_mode: wgpu::VertexStepMode::Vertex,
-        attributes: &[
-            // Position
-            wgpu::VertexAttribute {
-                format: wgpu::VertexFormat::Float32x3,
-                offset: 0,
-                shader_location: 0,
-            },
-            // Color
-            wgpu::VertexAttribute {
-                format: wgpu::VertexFormat::Float32x3,
-                offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                shader_location: 1,
-            },
-        ],
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                // Position
+                wgpu::VertexAttribute {
+                    format: wgpu::VertexFormat::Float32x3,
+                    offset: 0,
+                    shader_location: 0,
+                },
+                // Color
+                wgpu::VertexAttribute {
+                    format: wgpu::VertexFormat::Float32x3,
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                },
+            ],
+        }
     }
 }
