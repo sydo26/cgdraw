5a144419571865e84dc6fece528b4f10de976899
Vinicius <sydoafk@gmail.com>
2023-05-22 12:01:37 -0300

    feat: create input window events in window runner

--- crates/cgdraw_ui/src/window.rs
+++ crates/cgdraw_ui/src/window.rs
@@ -1,3 +1,4 @@
+use cgdraw_core::keyboard::keys::VirtualKeyCode;
 use winit::{dpi::PhysicalSize, event, event_loop};
 
 pub enum WindowEvent {
@@ -5,6 +6,7 @@ pub enum WindowEvent {
     Resize { size: PhysicalSize<u32> },
     Close,
     Redraw,
+    KeyPressed { key_code: VirtualKeyCode },
 }
 
 pub struct Window {
@@ -29,27 +31,73 @@ impl Window {
     where
         F: 'static + FnMut(WindowEvent),
     {
+        fn input_window_events(event: &event::WindowEvent) -> Option<WindowEvent> {
+            match event {
+                event::WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            state: event::ElementState::Pressed,
+                            virtual_keycode,
+                            ..
+                        },
+                    ..
+                } => {
+                    if let Some(vkc) = virtual_keycode {
+                        VirtualKeyCode::by_winit_keycode(*vkc)
+                            .map(|key_code| WindowEvent::KeyPressed { key_code })
+                    } else {
+                        None
+                    }
+                }
+
+                event::WindowEvent::Resized(physical_size) => Some(WindowEvent::Resize {
+                    size: *physical_size,
+                }),
+
+                event::WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    Some(WindowEvent::Resize {
+                        size: **new_inner_size,
+                    })
+                }
+
+                _ => None,
+            }
+        }
+
         self.event_loop.run(move |event, _, control_flow| {
             *control_flow = event_loop::ControlFlow::Wait;
 
             match event {
-                event::Event::RedrawEventsCleared => {
+                event::Event::MainEventsCleared => {
                     // Request redraw
                     self.window.request_redraw();
                 }
 
                 // Resize Event
                 event::Event::WindowEvent {
-                    event:
-                        event::WindowEvent::Resized(size)
-                        | event::WindowEvent::ScaleFactorChanged {
-                            new_inner_size: &mut size,
-                            ..
-                        },
+                    ref event,
                     window_id,
                 } if window_id == self.window.id() => {
-                    // Resize
-                    handler(WindowEvent::Resize { size });
+                    let result = input_window_events(event);
+
+                    if let Some(e) = result {
+                        handler(e)
+                    }
+
+                    match event {
+                        event::WindowEvent::CloseRequested
+                        | event::WindowEvent::KeyboardInput {
+                            input:
+                                event::KeyboardInput {
+                                    state: event::ElementState::Pressed,
+                                    virtual_keycode: Some(event::VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = event_loop::ControlFlow::Exit,
+
+                        _ => {}
+                    }
                 }
 
                 // Close Event
