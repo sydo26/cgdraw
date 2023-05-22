f340c80c61532e4c95c4401fdb464c8563ca6370
Vinicius <sydoafk@gmail.com>
2023-05-22 12:51:23 -0300

    feat: create vertex struct

---
+++ crates/cgdraw_core/src/graphic/vertex.rs
@@ -0,0 +1,29 @@
+#[repr(C)]
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct Vertex {
+    pub position: [f32; 3],
+    pub color: [f32; 3],
+}
+
+fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+    use std::mem;
+
+    wgpu::VertexBufferLayout {
+        array_stride: mem::size_of::<SimpleVertex>() as wgpu::BufferAddress,
+        step_mode: wgpu::VertexStepMode::Vertex,
+        attributes: &[
+            // Position
+            wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x3,
+                offset: 0,
+                shader_location: 0,
+            },
+            // Color
+            wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x3,
+                offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                shader_location: 1,
+            },
+        ],
+    }
+}
