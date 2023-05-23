eb68ceac7696a20f25cc2a197e29bc4b2ec2c55b
Vinicius <sydoafk@gmail.com>
2023-05-23 01:01:30 -0300

    refactor: add vertex buffer state in draw_vertices

--- crates/cgdraw_render/src/graphic/draw.rs
+++ crates/cgdraw_render/src/graphic/draw.rs
@@ -1,27 +1,22 @@
+use crate::VertexBufferState;
+
 pub trait Draw<'a> {
     /**
      * Desenha os vértices na tela.
      */
-    fn draw_vertices(
-        &mut self,
-        vertex_buffer: &'a wgpu::Buffer,
-        index_buffer: &'a wgpu::Buffer,
-        num_elements: u32,
-    );
+    fn draw_vertices(&mut self, buffer_state: &'a VertexBufferState);
 }
 
 impl<'a, 'b> Draw<'b> for wgpu::RenderPass<'a>
 where
     'b: 'a,
 {
-    fn draw_vertices(
-        &mut self,
-        vertex_buffer: &'a wgpu::Buffer,
-        index_buffer: &'a wgpu::Buffer,
-        num_elements: u32,
-    ) {
-        self.set_vertex_buffer(0, vertex_buffer.slice(..));
-        self.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-        self.draw_indexed(0..num_elements, 0, 0..1);
+    fn draw_vertices(&mut self, buffer_state: &'a VertexBufferState) {
+        self.set_vertex_buffer(0, buffer_state.vertex_buffer.slice(..));
+        self.set_index_buffer(
+            buffer_state.index_buffer.slice(..),
+            wgpu::IndexFormat::Uint16,
+        );
+        self.draw_indexed(0..buffer_state.num_elements, 0, 0..1);
     }
 }
