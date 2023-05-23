7b1d3ba6aea42a84636baba067ce524f3616a7c6
Vinicius <sydoafk@gmail.com>
2023-05-23 01:00:58 -0300

    feat: create state of the render crate

---
+++ crates/cgdraw_render/src/state.rs
@@ -0,0 +1,35 @@
+pub struct VertexBufferState {
+    pub vertex_buffer: wgpu::Buffer,
+    pub index_buffer: wgpu::Buffer,
+    pub num_elements: u32,
+}
+
+pub struct BuffersState {
+    /**
+     * Os buffers dos vertices que serão renderizados
+     */
+    pub vertices: Vec<VertexBufferState>,
+}
+
+pub struct RenderState {
+    /**
+     * Os buffers que serão renderizados
+     */
+    pub buffers: BuffersState,
+}
+
+impl Default for RenderState {
+    fn default() -> Self {
+        let buffers = BuffersState {
+            vertices: Vec::new(),
+        };
+
+        Self { buffers }
+    }
+}
+
+impl RenderState {
+    pub fn add_vertex_buffer_state(&mut self, vertex_buffer_state: VertexBufferState) {
+        self.buffers.vertices.push(vertex_buffer_state);
+    }
+}
