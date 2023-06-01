2583d2f74beb6ebd578dfb161683ae765e6e11ad
Vinicius <sydoafk@gmail.com>
2023-06-01 12:23:22 -0300

    feat: add target in initial attributes and change the goal of the rotations

--- crates/cgdraw_app/src/camera.rs
+++ crates/cgdraw_app/src/camera.rs
@@ -1,10 +1,13 @@
 pub struct CameraInitialAttributes {
-    // Posição inicial da câmera. (x, y, z);
+    /// Posição inicial da câmera. (x, y, z);
     pub position: (f32, f32, f32),
 
-    // Rotação vertical inicial da câmera em graus.
-    pub v_rotation: f32,
+    /// Ponto para onde a câmera deve apontar inicialmente. (x, y, z);
+    pub target: Option<(f32, f32, f32)>,
 
-    // Rotação horizontal inicial da câmera em graus.
-    pub h_rotation: f32,
+    /// Rotação vertical aplicada inicialmente após a câmera estar apontando para o ponto target informado.
+    pub v_rotation: Option<f32>,
+
+    /// Rotação horizontal aplicada inicialmente após a câmera estar apontando para o ponto target informado.
+    pub h_rotation: Option<f32>,
 }
