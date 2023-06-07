c627f1a776aefd3561ea3a433d0cfab5445002b6
Vinicius <sydoafk@gmail.com>
2023-06-07 19:57:17 -0300

    feat: update vectors impls

--- crates/cgdraw_math/src/vector.rs
+++ crates/cgdraw_math/src/vector.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::mem;
 use std::ops::*;
 
 use num_traits::{Float, Zero};
@@ -33,6 +35,38 @@ pub struct Vec4<T> {
     pub w: T,
 }
 
+impl<T: BaseNum> Vec3<T> {
+    /// Faz o produto cruzado entre dois vetores.
+    #[inline]
+    pub fn cross(self, other: Vec3<T>) -> Vec3<T> {
+        Vec3::new(
+            (self.y * other.z) - (self.z * other.y),
+            (self.z * other.x) - (self.x * other.z),
+            (self.x * other.y) - (self.y * other.x),
+        )
+    }
+
+    /// Cria um `Vec4` com os valores do vetor tridimensional e o valor `w`.
+    #[inline]
+    pub fn extend(self, w: T) -> Vec4<T> {
+        Vec4::new(self.x, self.y, self.z, w)
+    }
+
+    /// Cria um `Vec2` com os valores do vetor tridimensional, removendo o valor `z`.
+    #[inline]
+    pub fn truncate(self) -> Vec2<T> {
+        Vec2::new(self.x, self.y)
+    }
+}
+
+impl<T: BaseNum> Vec4<T> {
+    /// Cria um `Vec3` com os valores do vetor quadradimensional, removendo o valor `w`.
+    #[inline]
+    pub fn truncate(self) -> Vec3<T> {
+        Vec3::new(self.x, self.y, self.z)
+    }
+}
+
 macro_rules! impl_vector {
     ($V:ident { $($field:ident),+ }, $n:expr) => {
         impl<T> $V<T> {
@@ -56,6 +90,20 @@ macro_rules! impl_vector {
             #[inline]
             pub fn from_value(scalar: T) -> $V<T> {
                 $V { $($field: scalar),+ }
+            }
+
+            #[inline]
+            pub fn sum(self) -> T where T: Add<Output = T> {
+                fold_array!(add, { $(self.$field),+ })
+            }
+        }
+
+        impl<T: BaseNum + Neg<Output = T>> Neg for $V<T>  {
+            type Output = $V<T>;
+
+            #[inline]
+            fn neg(self) -> $V<T> {
+                $V::new($(-self.$field),+)
             }
         }
 
@@ -107,7 +155,37 @@ macro_rules! impl_vector {
             }
         }
 
+        impl<T: BaseNum> Mul<T> for $V<T> {
+            type Output = $V<T>;
 
+            fn mul(self, rhs: T) -> Self::Output {
+                $V::new($(self.$field * rhs),+)
+            }
+        }
+
+        impl<T: BaseNum> Div<T> for $V<T> {
+            type Output = $V<T>;
+
+            fn div(self, rhs: T) -> Self::Output {
+                $V::new($(self.$field / rhs),+)
+            }
+        }
+
+        impl<T: BaseNum> Add<T> for $V<T> {
+            type Output = $V<T>;
+
+            fn add(self, rhs: T) -> Self::Output {
+                $V::new($(self.$field + rhs),+)
+            }
+        }
+
+        impl<T: BaseNum> Sub<T> for $V<T> {
+            type Output = $V<T>;
+
+            fn sub(self, rhs: T) -> Self::Output {
+                $V::new($(self.$field - rhs),+)
+            }
+        }
     }
 }
 
@@ -115,3 +193,41 @@ impl_vector!(Vec1 { x }, 1);
 impl_vector!(Vec2 { x, y }, 2);
 impl_vector!(Vec3 { x, y, z }, 3);
 impl_vector!(Vec4 { x, y, z, w }, 4);
+
+impl_fixed_array_conversions!(Vec1<S> { x: 0 }, 1);
+impl_fixed_array_conversions!(Vec2<S> { x: 0, y: 1 }, 2);
+impl_fixed_array_conversions!(Vec3<S> { x: 0, y: 1, z: 2 }, 3);
+impl_fixed_array_conversions!(Vec4<S> { x: 0, y: 1, z: 2, w: 3 }, 4);
+
+impl_tuple_conversions!(Vec1<S> { x }, (S,));
+impl_tuple_conversions!(Vec2<S> { x, y }, (S, S,));
+impl_tuple_conversions!(Vec3<S> { x, y, z }, (S, S, S,));
+impl_tuple_conversions!(Vec4<S> { x, y, z, w }, (S, S, S, S,));
+
+impl<S: fmt::Debug> fmt::Debug for Vec1<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Vec1 ")?;
+        <[S; 1] as fmt::Debug>::fmt(self.as_ref(), f)
+    }
+}
+
+impl<S: fmt::Debug> fmt::Debug for Vec2<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Vec2 ")?;
+        <[S; 2] as fmt::Debug>::fmt(self.as_ref(), f)
+    }
+}
+
+impl<S: fmt::Debug> fmt::Debug for Vec3<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Vec3 ")?;
+        <[S; 3] as fmt::Debug>::fmt(self.as_ref(), f)
+    }
+}
+
+impl<S: fmt::Debug> fmt::Debug for Vec4<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Vec4 ")?;
+        <[S; 4] as fmt::Debug>::fmt(self.as_ref(), f)
+    }
+}
