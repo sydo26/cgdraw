3a252d585619460ece5bc78bf0f6cbc68fa7efc4
Vinicius <sydoafk@gmail.com>
2023-06-07 19:57:45 -0300

    feat: update dependency treen in math lib

--- crates/cgdraw_math/src/lib.rs
+++ crates/cgdraw_math/src/lib.rs
@@ -1,1 +1,13 @@
+pub mod rotate;
 pub mod translate;
+
+pub use rotate::*;
+
+pub use translate::*;
+
+pub mod angle;
+pub mod macros;
+pub mod matrix;
+pub mod num;
+pub mod point;
+pub mod vector;
