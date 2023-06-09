bff3caef13903f8a750d037c87ef8ff60654acf0
Vinicius <sydoafk@gmail.com>
2023-06-09 10:29:43 -0300

    feat: create news operations in matrix4x4

--- crates/cgdraw_math/src/matrix.rs
+++ crates/cgdraw_math/src/matrix.rs
@@ -5,6 +5,8 @@ use crate::{
     vector::{Vec3, Vec4},
 };
 
+use std::ops::*;
+
 use std::mem;
 
 /// Matriz 4x4, onde cada coluna é um vetor de 4 elementos.
@@ -41,6 +43,17 @@ impl<T> Matrix4x4<T> {
 }
 
 impl<T: BaseFloat> Matrix4x4<T> {
+    /// Retorna a linha `r` da matriz.
+    #[inline]
+    fn row(&self, r: usize) -> Vec4<T> {
+        let x = self[0][r];
+        let y = self[1][r];
+        let z = self[2][r];
+        let w = self[3][r];
+
+        Vec4::new(x, y, z, w)
+    }
+
     /// Cria uma matriz de transformação que mira a partir de um ponto de origem `eye`, para um
     /// vetor de direção `dir`, usando o vetor `up` como orientação de referência.
     pub fn look_at_rh(eye: Point3<T>, direction: Vec3<T>, up: Vec3<T>) -> Matrix4x4<T> {
@@ -85,12 +98,14 @@ impl<T: BaseFloat> Matrix4x4<T> {
             T::one(),
         );
 
+        let translation_matrix = Self::from_translate(Vec3::new(-eye.x, -eye.y, -eye.z));
+
         // Multiplicação da matriz de look-at com a matriz de translação
-        look_at_matrix * Self::from_translate(Vec3::new(-eye.x, -eye.y, -eye.z))
+        look_at_matrix * translation_matrix
     }
 
     /// Cria uma matriz identidade.
-    pub fn identity() -> Self {
+    pub fn identity() -> Matrix4x4<T> {
         Self::from_cols(
             Vec4::new(T::one(), T::zero(), T::zero(), T::zero()),
             Vec4::new(T::zero(), T::one(), T::zero(), T::zero()),
@@ -100,7 +115,7 @@ impl<T: BaseFloat> Matrix4x4<T> {
     }
 
     /// Cria uma matriz 4x4 de translação a partir de um vetor de 3 elementos (x, y, z).
-    pub fn from_translate(xyz: Vec3<T>) -> Self {
+    pub fn from_translate(xyz: Vec3<T>) -> Matrix4x4<T> {
         Self::from_cols(
             Vec4::new(T::one(), T::zero(), T::zero(), T::zero()),
             Vec4::new(T::zero(), T::one(), T::zero(), T::zero()),
@@ -119,7 +134,7 @@ impl<T: BaseFloat> Matrix4x4<T> {
     ///     [0, 0,   0,    1],
     /// ]
     /// ```
-    pub fn from_rotate_x(angle: Rad<T>) -> Self {
+    pub fn from_rotate_x(angle: Rad<T>) -> Matrix4x4<T> {
         let (sin, cos) = angle.0.sin_cos();
         Self::from_cols(
             Vec4::new(T::one(), T::zero(), T::zero(), T::zero()),
@@ -139,7 +154,7 @@ impl<T: BaseFloat> Matrix4x4<T> {
     ///     [0,    0, 0,    1],
     /// ]
     /// ```
-    pub fn from_rotate_y(angle: Rad<T>) -> Self {
+    pub fn from_rotate_y(angle: Rad<T>) -> Matrix4x4<T> {
         let (sin, cos) = angle.0.sin_cos();
         Self::from_cols(
             Vec4::new(cos, T::zero(), sin, T::zero()),
@@ -159,7 +174,7 @@ impl<T: BaseFloat> Matrix4x4<T> {
     ///     [0,    0,   0, 1],
     /// ]
     /// ```
-    pub fn from_rotate_z(angle: Rad<T>) -> Self {
+    pub fn from_rotate_z(angle: Rad<T>) -> Matrix4x4<T> {
         let (sin, cos) = angle.0.sin_cos();
         Self::from_cols(
             Vec4::new(cos, sin, T::zero(), T::zero()),
@@ -179,7 +194,7 @@ impl<T: BaseFloat> Matrix4x4<T> {
     ///     [0, 0, 0, 1],
     /// ]
     /// ```
-    pub fn from_scale(xyz: Vec3<T>) -> Self {
+    pub fn from_scale(xyz: Vec3<T>) -> Matrix4x4<T> {
         Self::from_cols(
             Vec4::new(xyz.x, T::zero(), T::zero(), T::zero()),
             Vec4::new(T::zero(), xyz.y, T::zero(), T::zero()),
@@ -189,9 +204,71 @@ impl<T: BaseFloat> Matrix4x4<T> {
     }
 }
 
-// macro_rules! impl_matrix {
-//     ($MatrixN:ident, $VectorN:ident { $($field:ident : $row_index:expr),+ }) => {};
-// }
+// Implementa a multiplicação de matrizes 4x4.
+impl<T: BaseFloat> Mul<Matrix4x4<T>> for Matrix4x4<T> {
+    type Output = Matrix4x4<T>;
+    fn mul(self, rhs: Matrix4x4<T>) -> Self::Output {
+        let a = self[0];
+        let b = self[1];
+        let c = self[2];
+        let d = self[3];
+
+        Matrix4x4::from_cols(
+            a * rhs[0][0] + b * rhs[0][1] + c * rhs[0][2] + d * rhs[0][3],
+            a * rhs[1][0] + b * rhs[1][1] + c * rhs[1][2] + d * rhs[1][3],
+            a * rhs[2][0] + b * rhs[2][1] + c * rhs[2][2] + d * rhs[2][3],
+            a * rhs[3][0] + b * rhs[3][1] + c * rhs[3][2] + d * rhs[3][3],
+        )
+    }
+}
+
+macro_rules! impl_matrix {
+    ($MatrixN:ident { $($field:ident),+ }, $n:expr) => {
+        impl<T: BaseFloat> Mul<T> for $MatrixN<T> {
+            type Output = $MatrixN<T>;
+
+            #[inline]
+            fn mul(self, rhs: T) -> $MatrixN<T> {
+                $MatrixN { $($field: self.$field * rhs),+ }
+            }
+        }
+    };
+}
+
+macro_rules! impl_matrix_vector_mul {
+    ($MatrixN:ident, $VectorN:ident { $($field:ident : $row_index:expr),+} ) => {
+        impl<T: BaseFloat> Mul<$VectorN<T>> for $MatrixN<T> {
+            type Output = $VectorN<T>;
+
+            #[inline]
+            fn mul(self, vector: $VectorN<T>) -> $VectorN<T> {
+                $VectorN::new($(self.row($row_index).dot(vector.clone())),+)
+            }
+        }
+    };
+}
+
+macro_rules! index_operators {
+    ($MatrixN:ident<$S:ident>, $n:expr, $Output:ty, $I:ty) => {
+        impl<$S> Index<$I> for $MatrixN<$S> {
+            type Output = $Output;
+
+            #[inline]
+            fn index<'a>(&self, i: $I) -> &$Output {
+                let v: &[[$S; $n]; $n] = self.as_ref();
+                From::from(&v[i])
+            }
+        }
+
+        impl<$S> IndexMut<$I> for $MatrixN<$S> {
+            #[inline]
+            fn index_mut<'a>(&mut self, i: $I) -> &mut $Output {
+                let v: &mut [[$S; $n]; $n] = self.as_mut();
+                From::from(&mut v[i])
+            }
+        }
+    };
+}
 
 macro_rules! fixed_array_conversions {
     ($MatrixN:ident <$S:ident> { $($field:ident : $index:expr),+ }, $n:expr) => {
@@ -270,4 +347,18 @@ macro_rules! fixed_array_conversions {
     }
 }
 
+impl_matrix!(Matrix4x4 { c0, c1, c2, c3 }, 4);
+
+impl_matrix_vector_mul!(
+    Matrix4x4,
+    Vec4 {
+        x: 0,
+        y: 1,
+        z: 2,
+        w: 3
+    }
+);
+
 fixed_array_conversions!(Matrix4x4<T> { c0: 0, c1: 1, c2: 2, c3: 3 }, 4);
+
+index_operators!(Matrix4x4<T>, 4, Vec4<T>, usize);
