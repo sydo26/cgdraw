721f4f4eb89bf9e2eb2bb33e09f8dc7eb899449b
Vinicius <sydoafk@gmail.com>
2023-06-09 10:29:10 -0300

    feat: create macro for impl camera uniform

--- crates/cgdraw_camera/src/uniform.rs
+++ crates/cgdraw_camera/src/uniform.rs
@@ -9,54 +9,60 @@ pub struct CameraUniformFloat32 {
 }
 
 impl Default for CameraUniformFloat32 {
-    fn default() -> Self {
-        Self {
-            view_position: [0.0; 4],
+    fn default() -> CameraUniformFloat32 {
+        CameraUniformFloat32 {
             view_proj: Matrix4x4::identity().into(),
+            view_position: [0.0; 4],
         }
     }
 }
 
-impl CameraUniformFloat32 {
-    /// Cria um buffer da câmera para enviar para o shader.
-    pub fn create_buffer(device: &wgpu::Device, uniform: Self) -> wgpu::Buffer {
-        device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Camera Buffer"),
-            contents: bytemuck::cast_slice(&[uniform]),
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        })
-    }
+macro_rules! impl_camera_uniform {
+    ($CameraUniformN:ty) => {
+        impl $CameraUniformN {
+            /// Cria um buffer da câmera para enviar para o shader.
+            pub fn create_buffer(device: &wgpu::Device, uniform: Self) -> wgpu::Buffer {
+                device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Camera Buffer"),
+                    contents: bytemuck::cast_slice(&[uniform]),
+                    usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+                })
+            }
 
-    /// Cria um `bind_group_layout` para a câmera, que será usado para criar um `bind_group`.
-    pub fn create_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
-        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            }],
-            label: Some("camera_bind_group_layout"),
-        })
-    }
+            /// Cria um `bind_group_layout` para a câmera, que será usado para criar um `bind_group`.
+            pub fn create_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
+                device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    entries: &[wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    }],
+                    label: Some("camera_bind_group_layout"),
+                })
+            }
 
-    /// Cria um `bind_group` para a câmera, que será usado pelo `render_pipeline`.
-    pub fn create_bind_group(
-        device: &wgpu::Device,
-        layout: &wgpu::BindGroupLayout,
-        buffer: &wgpu::Buffer,
-    ) -> wgpu::BindGroup {
-        device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: buffer.as_entire_binding(),
-            }],
-            label: Some("camera_bind_group"),
-        })
-    }
+            /// Cria um `bind_group` para a câmera, que será usado pelo `render_pipeline`.
+            pub fn create_bind_group(
+                device: &wgpu::Device,
+                layout: &wgpu::BindGroupLayout,
+                buffer: &wgpu::Buffer,
+            ) -> wgpu::BindGroup {
+                device.create_bind_group(&wgpu::BindGroupDescriptor {
+                    layout,
+                    entries: &[wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: buffer.as_entire_binding(),
+                    }],
+                    label: Some("camera_bind_group"),
+                })
+            }
+        }
+    };
 }
+
+impl_camera_uniform!(CameraUniformFloat32);
