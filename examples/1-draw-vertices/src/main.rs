8ff03ab01b72438c87ea4cec50a10a2097756da9
Vinicius <sydoafk@gmail.com>
2023-06-22 02:48:22 -0300

    fix: draw vertices example

--- examples/1-draw-vertices/src/main.rs
+++ examples/1-draw-vertices/src/main.rs
@@ -1,34 +1,34 @@
-use cgdraw::{event::AppEvent, App, CameraInitialAttributes, Color, Projection};
+use cgdraw::{event::AppEvent, AppBuilder, Color};
 
 fn main() {
-    let app = App::default();
+    let builder = AppBuilder::default();
 
-    app.add_camera(
-        "camera",
-        CameraInitialAttributes {
-            position: (0.0, 0.0, -1.0),
-            v_rotation: 90.0,
-            h_rotation: 0.0,
-        },
-    )
-    .initial_camera("camera")
-    .camera_projection(Projection::new(120.0, 0.1, 100.0))
-    .run(move |event| match event {
+    let app = builder.build();
+
+    app.run(move |event| match event {
         AppEvent::Setup => {
             println!("Setup!")
         }
 
-        AppEvent::Update => {}
+        AppEvent::Update { .. } => {}
 
         AppEvent::Draw { graphics } => {
             graphics.color(Color::RED);
             graphics.save_vertice(-0.5, 0.5, 0.0);
+            graphics.color(Color::YELLOW);
             graphics.save_vertice(0.5, 0.5, 0.0);
             graphics.color(Color::BLUE);
             graphics.save_vertice(0.5, -0.5, 0.0);
+            graphics.color(Color::SILVER);
             graphics.save_vertice(-0.5, -0.5, 0.0);
 
-            graphics.draw([0, 1, 2, 2, 1, 0, 0, 2, 3, 3, 2, 0].to_vec());
+            graphics.draw(
+                [
+                    0, 1, 2, 2, 1, 0, // TRIANGULO 01
+                    0, 2, 3, 3, 2, 0, // TRIANGULO 02
+                ]
+                .to_vec(),
+            );
         }
 
         AppEvent::KeyPressed { .. } => {
