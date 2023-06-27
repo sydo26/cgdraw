f47a668afa0b50f9316fe0e566ff5e24b5c02d33
Vinicius <sydoafk@gmail.com>
2023-06-27 20:43:53 -0300

    refactor: remove perspective, opengl and ortographic matrix functions of the impl

--- crates/cgdraw_core/src/projection/mod.rs
+++ crates/cgdraw_core/src/projection/mod.rs
@@ -1,84 +1,89 @@
 use cgdraw_math::{angle::Rad, matrix::Matrix4x4, num::BaseFloat};
 use num_traits::cast;
 
-pub struct Projection<T> {
-    /**
-     * Aspecto que define a proporção
-     * entre a largura e a altura da tela
-     */
-    pub aspect: T,
+fn opengl_to_wgpu_matrix<T: BaseFloat>() -> Matrix4x4<T> {
+    let half: T = cast(0.5).unwrap();
 
-    /**
-     * Define o ângulo de visão da projeção
-     */
-    pub fovy: Rad<T>,
+    #[rustfmt::skip]
+    let matrix = Matrix4x4::new(
+        T::one(), T::zero(), T::zero(), T::zero(),
+        T::zero(), T::one(), T::zero(), T::zero(),
+        T::zero(), T::zero(), half, T::zero(),
+        T::zero(), T::zero(), half, T::one(),
+    );
 
-    /**
-     * Define a distância do plano de visão
-     * mais próximo da origem da projeção
-     */
-    pub znear: T,
-
-    /**
-     * Define a distância do plano de visão
-     * mais distante da origem da projeção
-     */
-    pub zfar: T,
+    matrix
 }
 
-impl<T: BaseFloat> Projection<T> {
-    pub fn new<F: Into<Rad<T>>>(fovy: F, znear: T, zfar: T) -> Self {
-        Self {
-            aspect: T::one(),
-            fovy: fovy.into(),
-            znear,
-            zfar,
-        }
-    }
+pub fn perspective<T: BaseFloat>(aspect: T, fovy: Rad<T>, znear: T, zfar: T) -> Matrix4x4<T> {
+    let two: T = cast(2).unwrap();
+    let f = Rad::cot(fovy / two);
 
-    pub fn resize(&mut self, width: u32, height: u32) {
-        self.aspect = T::from(width).unwrap() / T::from(height).unwrap();
-    }
+    let c0r0 = f / aspect;
+    let c0r1 = T::zero();
+    let c0r2 = T::zero();
+    let c0r3 = T::zero();
 
-    pub fn perspective(&self) -> Matrix4x4<T> {
-        let half: T = cast(0.5).unwrap();
-        #[rustfmt::skip]
-        let opengl_to_wgpu_matrix = Matrix4x4::new(
-            T::one(), T::zero(), T::zero(), T::zero(),
-            T::zero(), T::one(), T::zero(), T::zero(),
-            T::zero(), T::zero(), half, T::zero(),
-            T::zero(), T::zero(), half, T::one(),
-        );
+    let c1r0 = T::zero();
+    let c1r1 = f;
+    let c1r2 = T::zero();
+    let c1r3 = T::zero();
 
-        let two: T = cast(2).unwrap();
-        let frad: Rad<T> = self.fovy / two;
-        let f = Rad::cot(frad);
+    let c2r0 = T::zero();
+    let c2r1 = T::zero();
+    let c2r2 = (zfar + znear) / (znear - zfar);
+    let c2r3 = -T::one();
 
-        let c0r0 = f / self.aspect;
-        let c0r1 = T::zero();
-        let c0r2 = T::zero();
-        let c0r3 = T::zero();
+    let c3r0 = T::zero();
+    let c3r1 = T::zero();
+    let c3r2 = (two * zfar * znear) / (znear - zfar);
+    let c3r3 = T::zero();
 
-        let c1r0 = T::zero();
-        let c1r1 = f;
-        let c1r2 = T::zero();
-        let c1r3 = T::zero();
+    #[rustfmt::skip]
+    let perspective_matrix = Matrix4x4::new(
+        c0r0, c0r1, c0r2, c0r3,
+        c1r0, c1r1, c1r2, c1r3,
+        c2r0, c2r1, c2r2, c2r3,
+        c3r0, c3r1, c3r2, c3r3,
+    );
 
-        let c2r0 = T::zero();
-        let c2r1 = T::zero();
-        let c2r2 = (self.zfar + self.znear) / (self.znear - self.zfar);
-        let c2r3 = -T::one();
+    opengl_to_wgpu_matrix() * perspective_matrix
+}
 
-        let c3r0 = T::zero();
-        let c3r1 = T::zero();
-        let c3r2 = (two * self.zfar * self.znear) / (self.znear - self.zfar);
-        let c3r3 = T::zero();
+pub fn orthographic<T: BaseFloat>(
+    left: T,
+    right: T,
+    bottom: T,
+    top: T,
+    znear: T,
+    zfar: T,
+) -> Matrix4x4<T> {
+    let two: T = cast(2).unwrap();
 
-        let perspective_matrix = Matrix4x4::new(
-            c0r0, c0r1, c0r2, c0r3, c1r0, c1r1, c1r2, c1r3, c2r0, c2r1, c2r2, c2r3, c3r0, c3r1,
-            c3r2, c3r3,
-        );
+    let c0r0 = two / (right - left);
+    let c0r1 = T::zero();
+    let c0r2 = T::zero();
+    let c0r3 = T::zero();
 
-        opengl_to_wgpu_matrix * perspective_matrix
-    }
+    let c1r0 = T::zero();
+    let c1r1 = two / (top - bottom);
+    let c1r2 = T::zero();
+    let c1r3 = T::zero();
+
+    let c2r0 = T::zero();
+    let c2r1 = T::zero();
+    let c2r2 = -two / (zfar - znear);
+    let c2r3 = T::zero();
+
+    let c3r0 = -(right + left) / (right - left);
+    let c3r1 = -(top + bottom) / (top - bottom);
+    let c3r2 = -(zfar + znear) / (zfar - znear);
+    let c3r3 = T::one();
+
+    let ortho_matrix = Matrix4x4::new(
+        c0r0, c0r1, c0r2, c0r3, c1r0, c1r1, c1r2, c1r3, c2r0, c2r1, c2r2, c2r3, c3r0, c3r1, c3r2,
+        c3r3,
+    );
+
+    opengl_to_wgpu_matrix() * ortho_matrix
 }
